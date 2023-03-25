1da17a51b3c441cb4f14e81c4a8f10e47cb6b450
Jeremy Karagania <jeremykaragania@icloud.com>
2023-03-25 09:35:41 +0800

    Add literal copy assignment operator

--- sit/details/literal.h
+++ sit/details/literal.h
@@ -8,6 +8,12 @@ namespace sit {
   public:
     literal(variable& x, const bool is_complemented = 0) noexcept : _variable(x), _is_complemented(is_complemented) {}
 
+    literal operator=(const literal& rhs) noexcept {
+      _is_complemented = rhs._is_complemented;
+      _variable = rhs._variable;
+      return *this;
+    }
+
     operator bool() const {
       return _is_complemented != _variable;
     }
