43bd0230c6e95b21be8b97c90878fe0a5b333ca5
Jeremy Karagania <jeremykaragania@icloud.com>
2023-04-22 13:53:18 +0800

    Change DIMACS format parser into function

--- sit/details/dimacs.h
+++ sit/details/dimacs.h
@@ -3,70 +3,48 @@
 
 #include <fstream>
 #include <limits>
-#include <details/formula.h>
 #include <string>
+#include <details/formula.h>
 
 namespace sit {
-  class dimacs {
-    public:
-    dimacs(std::string filename) {
-      std::ifstream ifs(filename, std::ios::in);
-      if (!ifs.is_open()) {
-        throw;
+  void read_dimacs(std::string filename, formula& init_formula, std::vector<variable>& init_variables) {
+    std::ifstream ifs(filename, std::ios::in);
+    if (!ifs.is_open()) {
+      throw;
+    }
+    while (1) {
+      if ((char)ifs.peek() == 'p') {
+        break;
       }
+      ifs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    ifs.ignore(std::numeric_limits<std::streamsize>::max(), ' ');
+    std::string buffer;
+    ifs >> buffer;
+    if (buffer != "cnf") {
+      throw;
+    }
+    ifs >> buffer;
+    std::size_t variable_number = std::stoull(buffer);
+    init_variables.resize(variable_number);
+    ifs >> buffer;
+    std::size_t clause_number = std::stoull(buffer);
+    init_formula.clauses().resize(clause_number);
+    for (clause& i : init_formula.clauses()) {
       while (1) {
-        if ((char)ifs.peek() == 'p') {
+        ifs >> buffer;
+        if (buffer == "0" || ifs.eof()) {
           break;
         }
-        ifs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-      }
-      ifs.ignore(std::numeric_limits<std::streamsize>::max(), ' ');
-      std::string buffer;
-      ifs >> buffer;
-      if (buffer != "cnf") {
-        throw;
-      }
-      ifs >> buffer;
-      std::size_t variable_number = std::stoull(buffer);
-      _variables.resize(variable_number);
-      ifs >> buffer;
-      std::size_t clause_number = std::stoull(buffer);
-      _formula.clauses().resize(clause_number);
-      for (clause& i : _formula.clauses()) {
-        while (1) {
-          ifs >> buffer;
-          if (buffer == "0" || ifs.eof()) {
-            break;
-          }
-          bool complemented = 0;
-          if (buffer[0] == '-') {
-            complemented = 1;
-            buffer.erase(buffer.begin());
-          }
-          i.literals().push_back({_variables[std::stoull(buffer) - 1], complemented});
+        bool complemented = 0;
+        if (buffer[0] == '-') {
+          complemented = 1;
+          buffer.erase(buffer.begin());
         }
+        i.literals().push_back({init_variables[std::stoull(buffer) - 1], complemented});
       }
     }
-
-    const formula& data() const noexcept {
-      return _formula;
-    }
-
-    formula& data() noexcept{
-      return _formula;
-    }
-
-    const std::vector<variable>& variables() const noexcept {
-      return _variables;
-    }
-
-    std::vector<variable>& variables() noexcept {
-      return _variables;
-    }
-    private:
-      formula _formula;
-      std::vector<variable> _variables;
-  };
+  }
 }
 
 #endif
